4b8875767b5e08d349b3842d2a81f57295c1e0cb
Philip Taylor <phitaylr@gmail.com>
2021-02-13 09:00:43 -0600

    remove test output from template

--- TEMPLATE.java
+++ TEMPLATE.java
@@ -4,17 +4,3 @@ public class TEMPLATE{
        System.out.println("three");
    }
 }
-
- // Running 1 JUnit test.
- // 
- // class Lab
- // 
- // User:			phita
- // Time:			2021/01/11 15:42:28
- // Dir:			C:\Users\phita\Desktop\JUnit Tests
- // JAVAHome:	C:\Program Files\Java\jdk-14.0.1
- // JAVA Ven:	Oracle Corporation
- // OS Arch:		amd64
- // OS Name:		Windows 10
- // 
- // Completed 1 tests  1 passed
